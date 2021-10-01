79957caa610fc904a39d1e6a252fa59de2b352f2
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-10-01 18:30:03 +0700

    Person version 1

---
+++ Person.java
@@ -0,0 +1,18 @@
+public class Person{
+    protected String name;
+    protected String lastname;
+    protected int  id;
+    protected String tel;
+    protected String email;
+
+
+
+    public Person(String name , String lastname ,int id , String tel , String email ) {
+        this.name = name;
+        this.lastname = lastname;
+        this.id = id;
+        this.tel = tel;
+        this.email = email;
+    }
+    
+}
