e28f0bfa4ea1f12efbb729ce2c157ec0a449460f
Louis Thanawin <kannaphongi62@nu.ac.th>
2021-10-01 22:36:21 +0700

    Update Version 2

--- Menu.java
+++ Menu.java
@@ -1,12 +1,10 @@
-public class Menu { // Big class
-   protected int id;
+public class Menu { 
    protected String menu_name;
    protected String type;
    protected int menu_price;
 
-   public Menu(int id,String menu_name,String type,int menu_price)
+   public Menu(String menu_name,String type,int menu_price)
    {
-       this.id = id;
        this.menu_name = menu_name;
        this.type = type;
        this.menu_price = menu_price;
